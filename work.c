a7c8c442813b06b74087373fb149e7d997cc5e50
Striveee <52830939+Striveee@users.noreply.github.com>
2019-08-05 19:55:35 +0800

    Update work.c

--- work.c
+++ work.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+int max(int x, int y)
+{
+    return x>y?x:y;
+}
+
 int main()
 {
+    int a = 10;
+    int b = 20;
+    int c = max(10, 20);
+    printf("%d\n", c);
     printf("hello world\n");
     return 0;   
 }
