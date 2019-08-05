f647e2ae759d762bc5760eb918ef76c026701e27
liangtiantian <1071342903@qq.com>
2019-08-05 20:51:56 +0800

    ‘添加了add方法’

--- work.c
+++ work.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+//第2个版本
 int max(int x, int y)
 {
     return x>y?x:y;
+}
+
+//第3个版本
+int add(int x, int y)
+{
+    return x+y;
 }
 
 int main()
@@ -10,7 +17,10 @@ int main()
     int a = 10;
     int b = 20;
     int c = max(10, 20);
+    int d = add(10, 20);
     printf("%d\n", c);
+    printf("%d\n", d);
+    //第1个版本
     printf("hello world\n");
     return 0;   
 }
